9cc33cb12f995ca7cd230abda54662d10d1442e8
JuanCarlos Roa Jr <roajrjuancarlos@hotmail.com>
2023-02-10 16:41:50 -0500

    userRoute code added

---
+++ routes/api/userRoute.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+//users
+router.route('/')
+    .get(getAllUsers)
+    .post(createUser);
+//users id
+router.route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+// add/delete friend
+router.route('/:id/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
+
+    module.exports = router;
